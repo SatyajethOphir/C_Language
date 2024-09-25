7b9c7274a472d4b7f32e9c43854b7e68047a5867
SOP <deveoper.sop111@gmail.com>
2024-09-25 21:02:45 +0530

    Number Reverse

---
+++ first.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+  int result, inp;
+  printf("Enter the Number");
+scanf("%d",&inp);
+result= reverse(inp);
+printf("The Reversed No Is %d",result);
+    return 0;
+} 
+int reverse(int n ){
+  int rev = 0 ;
+  int dig ;
+  while (n!=0)
+  {
+    dig = n%10 ; 
+    rev =rev *10 +dig;
+    n/=10;
+  }
+  return rev;
+}
